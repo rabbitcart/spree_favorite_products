4a01e967ecf12e4fd3b6b67c1297b7a283252044
Jason Kroll <jason@coffeeandhops.com>
2019-11-09 15:17:48 +1000

    Check for frontend
    Don't install frontend if not included in app

--- lib/generators/spree_favorite_products/install/install_generator.rb
+++ lib/generators/spree_favorite_products/install/install_generator.rb
@@ -5,13 +5,17 @@ module SpreeFavoriteProducts
       class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require store/spree_favorite_products\n"
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require spree/frontend/spree_favorite_products\n"
+        if SpreeExtension::ComponentsChecker.frontend_available?
+          append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require store/spree_favorite_products\n"
+          append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require spree/frontend/spree_favorite_products\n"
+        end
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "\n//= require admin/spree_favorite_products\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/spree_favorite_products\n", before: /\*\//, verbose: true
+        if SpreeExtension::ComponentsChecker.frontend_available?
+          inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/spree_favorite_products\n", before: /\*\//, verbose: true
+        end
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/spree_favorite_products\n", before: /\*\//, verbose: true
       end
 
